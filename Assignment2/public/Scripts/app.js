4d3ff7320c260f495d36af91231d15f653fea479
Devisveryfunny <dev.parmar@ontariotechu.net>
2024-11-07 23:14:53 -0500

    yes

--- Assignment2/public/Scripts/app.js
+++ Assignment2/public/Scripts/app.js
@@ -1,9 +1,10 @@
-
-// IIFE --> Immediately invoked function expression
-
-(function (){
-    function Start(){
-        console.log("App Started");
+// IIFE (Immediately Invoked Function Expression)
+(function () {
+    // Function that runs when the page has fully loaded
+    function Start() {
+        console.log("App Started");  // Simple log for testing
     }
+
+    // Attach event listener to the window load event
     window.addEventListener("load", Start);
 })();
--- Assignment2/routes/index.js
+++ Assignment2/routes/index.js
@@ -1,29 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Route for the home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* about me page. */
+// Route for the about me page
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
-/* GET projects page. */
+// Route for the projects page
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
-/* GET contact me page. */
+// Route for the contact me page
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
